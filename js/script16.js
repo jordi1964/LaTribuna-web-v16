94a620f6edfe7cca713bd27f422d15526b4eeb47
Jordi Sala <jordisalarodo@gmail.com>
2019-01-18 20:44:56 +0100

    Pàgina web de La Tribuna/El Pou de Llum, versió 16
    Pàgina web de La Tribuna/El Pou de Llum, versió 16

---
+++ js/script16.js
@@ -0,0 +1,57 @@
+/*
+Projecte LA TRIBUNA v16- JavaScript
+========================================== */
+
+
+/** Botó-Imago flotant lateral dret **/
+/* En fer click funciona com a interruptor mostrant i amagant barra flotant */
+
+function openNav() {
+  document.getElementById('barra-flot').style.display = "block";
+  document.getElementById('logo-menu').style.display = "none";
+}
+
+function closeNav() {
+  document.getElementById('barra-flot').style.display = "none";
+  document.getElementById('logo-menu').style.display = "block";
+}
+
+function openEquip1() {
+  document.getElementById('enric-text').style.display = "block";
+}
+
+function closeEquip1() {
+  document.getElementById('enric-text').style.display = "none";
+}
+
+function openEquip2() {
+  document.getElementById('nuria-text').style.display = "block";
+}
+
+function closeEquip2() {
+  document.getElementById('nuria-text').style.display = "none";
+}
+
+function openEquip3() {
+  document.getElementById('marc-text').style.display = "block";
+}
+
+function closeEquip3() {
+  document.getElementById('marc-text').style.display = "none";
+}
+
+function openEquip4() {
+  document.getElementById('remei-text').style.display = "block";
+}
+
+function closeEquip4() {
+  document.getElementById('remei-text').style.display = "none";
+}
+
+function openEquip5() {
+  document.getElementById('pere-text').style.display = "block";
+}
+
+function closeEquip5() {
+  document.getElementById('pere-text').style.display = "none";
+}
